62e6ee38d571c4a5b383c1823672f23f85123195
KM0912 <61630275+KM0912@users.noreply.github.com>
2021-11-24 23:47:25 +0900

    罫線を引く処理をモジュール化

--- create_file_list.py
+++ create_file_list.py
@@ -11,6 +11,34 @@ SHEET_TITLE = settings.sheet_title      # シート名
 ROW_INI     = settings.row_ini          # 行の初期位置
 COL_INI     = settings.col_ini          # 列の初期位置
 
+# フォルダ名/ファイル名が記入されている列より左側か右側かを判定するためのフラグの値
+LEFT_CELL   = 0
+RIGHT_CELL  = 1
+
+# 罫線を引く処理
+def draw_boader (start_row, end_row, start_col, end_col) :
+    for row in range(start_row, end_row + 1):
+
+        cell_flag = LEFT_CELL   # フォルダ名/ファイル名が記入されている列より左側か右側かを判定するためのフラグ
+        for col in range(start_col, end_col + 1):
+
+            # セルが空でなければ上罫線/左罫線を引く
+            if (sheet.cell(row=row, column=col).value != None):
+                sheet.cell(row=row, column=col).border = Border(top=Side(style='thin', color='000000'), left=Side(style='thin', color='000000'))
+
+                #　１列目でなければ
+                # if col != 1:
+                #     for col_i in range(col-1, 0, -1):
+                #         sheet.cell(row=row, column=col_i).border = Border(left=Side(style='thin', color='000000'))
+
+                cell_flag = RIGHT_CELL  # フォルダ名/ファイル名が記入されている列の罫線を引く処理が完了したので、フラグを更新
+
+            # セルが空であり、フォルダ名/ファイル名が記入されている列より右側のセルの場合、上罫線のみを引く
+            elif (cell_flag == RIGHT_CELL):
+                sheet.cell(row=row, column=col).border = Border(top=Side(style='thin', color='000000'))
+
+
+
 # OSを判定し、ファイルパスの区切り文字を決定
 if os.name == "nt":         # Windows
     delimiter = "¥"
@@ -49,24 +77,8 @@ for file in f_list:
         row += 1        # 次の行に移動
         col = COL_INI   # 列の位置を初期位置に移動
 
-# 枠線を引く処理
-for row in range(ROW_INI, sheet.max_row + 1):
-    flag = 0
-
-    for col in range(COL_INI, 10):
-
-        if (sheet.cell(row=row, column=col).value != None):
-            sheet.cell(row=row, column=col).border = Border(top=Side(style='thin', color='000000'), left=Side(style='thin', color='000000'))
-
-            if col != 1:
-                for col_i in range(col-1, 0, -1):
-                    sheet.cell(row=row, column=col_i).border = Border(left=Side(style='thin', color='000000'))
-
-            flag = 1    #
-
-        elif (flag == 1):
-            sheet.cell(row=row, column=col).border = Border(
-                top=Side(style='thin', color='000000'))
+# 罫線を引く処理
+draw_boader(ROW_INI, sheet.max_row, COL_INI, sheet.max_column)
 
 # 背景色
 fill = PatternFill(patternType='solid', fgColor='ffffff')
@@ -82,3 +94,4 @@ book.close()
 # TODO: 自分自身はファイルリストに含めない
 # TODO: oldファイルは含めない
 # TODO: 一番最後の行を含めない
+# TODO:モジュール化する
